cfc2e8097a35fdd01283577b35283f73222683ea
elgato <amzfba.1bestbuy@gmail.com>
2022-08-24 03:08:53 +0200

    images sizing

---
+++ js/right_click.js
@@ -0,0 +1,4 @@
+$(document).bind("contextmenu", function () {
+  alert("All contents in this website is copyrighed");
+  return false;
+});
--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const maps = document.getElementById("maps");
 const location1 = document.getElementById("location1");
 const location2 = document.getElementById("location2");
 const location3 = document.getElementById("location3");
+const footerLinkEl1 = document.getElementById("nofollow1");
+const footerLinkEl2 = document.getElementById("nofollow2");
 
 console.log(maps, location1, location2, location3);
 
@@ -17,4 +19,10 @@ location2.addEventListener("click", (ev) => {
 location3.addEventListener("click", (ev) => {
   ev.preventDefault();
   maps.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d251738.50631630272!2d18.60505198452243!3d-34.00296414904738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc516f59f60035%3A0x27935b3a64514879!2s111%20Kruis%20Rd%2C%20Brackenfell%2C%20Cape%20Town%2C%207925%2C%20Afrique%20du%20Sud!5e0!3m2!1sfr!2sfr!4v1661289629296!5m2!1sfr!2sfr" width="1440" height="700" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+});
+footerLinkEl1.addEventListener("click", (ev) => {
+  ev.preventDefault();
+});
+footerLinkEl2.addEventListener("click", (ev) => {
+  ev.preventDefault();
 });
