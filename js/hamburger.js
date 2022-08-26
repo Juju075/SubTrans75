837b7dca434f98fee9d036c21f226c0b7d8b254e
elgato <amzfba.1bestbuy@gmail.com>
2022-08-26 20:21:50 +0200

    Integration javascript and styling almost done :)

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,27 +1,10 @@
-let hamburgerEl = document.querySelector(".hamburger-container");
-const navLinks = document.getElementById("nav-links");
-console.log("🚀 ~ file: hamburger.js ~ line 3 ~ navLinks", navLinks);
-
-/** click sur icone 
-hamburgerEl.addEventListener("click", () => {
-  hamburgerEl.classList.toggle("active");
-});
-*/
+const hamburgerEl = document.querySelector(".hamburger-container"),
+  navLinks = document.getElementById("nav-links"),
+  hideForhamburger = document.getElementById("hide-for hamburger");
 
 hamburgerEl.addEventListener("click", () => {
   hamburgerEl.classList.toggle("active");
-
-  /** Activation du Menu css*/
   navLinks.classList.toggle("mobile-menu");
-});
-
-/**click sur icone */
-
-console.log(menuHamburger, navLinks);
-
-/**1- onclick */
-menuHamburger.addEventListener("click", () => {
-  //manque animation hamburger
-  /* classList */
-  navLinks.classList.toggle("mobile-menu");
+  hideForhamburger.classList.toggle("hide");
+  //
 });
